f7460b7508f6daa346dee5ba435938d26f816be9
JosueSaenz <josuesaenz885@gmail.com>
2023-01-05 10:46:47 -0500

    hola2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { Componente2Component } from './componentes/componente2/componente2.comp
 import { Componente3Component } from './componentes/componente3/componente3.component';
 import { Componente4Component } from './componentes/componente4/componente4.component';
 import { Componente5Component } from './componentes/componente5/componente5.component';
+import { Componente6Component } from './componentes/componente6/componente6.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { Componente5Component } from './componentes/componente5/componente5.comp
     Componente2Component,
     Componente3Component,
     Componente4Component,
-    Componente5Component
+    Componente5Component,
+    Componente6Component
   ],
   imports: [
     BrowserModule,
